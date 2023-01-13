46a2638801d721dbeed3ffe9252b7acb622b859a
aadil494 <8232aadil@gmail.com>
2023-01-14 00:44:55 +0530

    top car carousel added

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -1,50 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Car } from "../../components/car";
 import { ICar } from "../../../typings/car";
-
-
+import Carousel, {Dots,slidesToShowPlugin} from '@brainhubeu/react-carousel';
+import "@brainhubeu/react-carousel/lib/style.css";
 const TopCarsContainer = styled.div`
-
-    ${tw`
-        max-w-lg 
-        w-full 
-        flex 
-        flex-col 
-        items-center 
-        justify-center 
-        pr-4
-        pl-4
-        md:pl-0
-        md:pr-0 
-        mb-10
-    `};
+  ${tw`
+    max-w-screen-lg
+    w-full
+    flex
+    flex-col
+    items-center
+    justify-center
+    pr-4
+    pl-4
+    md:pl-0
+    md:pr-0
+    mb-10
+  `};
 `;
 
-const Title = styled.h1`
-    ${tw`
-        text-black 
-        text-2xl  
-        md:text-5xl 
-        font-extrabold 
-        md:font-black 
-        md:leading-normal 
-        `};
-    `;
+const Title = styled.h2`
+  ${tw`
+    text-3xl
+    lg:text-5xl
+    text-black
+    font-extrabold
+  `};
+`;
 
 const CarsContainer = styled.div`
-    ${tw`
-        flex  
-        justify-center 
-        mt-7 
-        md:mt-10 
+  ${tw`
+    w-full
+    flex
+    flex-wrap
+    justify-center
+    mt-7
+    md:mt-10
+  `};
+`;
 
-    `}
-`
+const EmptyCars = styled.div`
+  ${tw`
+    w-full
+    flex
+    justify-center
+    items-center
+    text-sm
+    text-gray-500
+  `};
+`;
+
+const LoadingContainer = styled.div`
+  ${tw`
+    w-full
+    mt-9
+    flex
+    justify-center
+    items-center
+    text-base
+    text-black
+  `};
+`;
+
 
 export function TopCars(){
 
+    const [current, setCurrent] = useState(0);
     const testCar: ICar = {
         name: "Audi S3 Car",
         mileage: "10k",
@@ -70,9 +93,52 @@ export function TopCars(){
     return <TopCarsContainer>
         <Title>Explore Our Top Deals</Title>
         <CarsContainer>
-            <Car  {...testCar}  />
-            <Car  {...testCar2}  />
-            <Car  {...testCar}  />
+        <Carousel
+            value={current}
+            onChange={setCurrent}
+            slides={
+                [
+                    <Car {...testCar}/>,
+                    <Car {...testCar2}/>,
+                    <Car {...testCar}/>,
+                    <Car {...testCar2}/>,
+                    <Car {...testCar}/>,
+
+                ]
+            }
+            plugins={[
+              "clickToChange",
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 3,
+                },
+              },
+            ]}
+            breakpoints={{
+              640: {
+                plugins: [
+                  {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                      numberOfSlides: 1,
+                    },
+                  },
+                ],
+              },
+              900: {
+                plugins: [
+                  {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                      numberOfSlides: 2,
+                    },
+                  },
+                ],
+              },
+            }}
+          />
+          <Dots value={current} onChange={setCurrent} number={3} />
         </CarsContainer>
     </TopCarsContainer>
 }
